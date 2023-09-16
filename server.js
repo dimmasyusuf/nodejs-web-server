8bf2334d5bd4f8fdd12bda63cb0458d4c4d07bc7
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-16 09:06:51 +0700

    feat: create http server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Halo, saya akan belajar membuat server');
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+
+  response.statusCode = 200;
+  response.end('<h1>Hello, World!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
