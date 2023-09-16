0f1263d5278104345c195c78e210646f5bd7b22c
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-16 09:17:47 +0700

    feat: method request

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-
   response.statusCode = 200;
-  response.end('<h1>Hello, World!</h1>');
+
+  const { method } = request;
+
+  if (method === 'GET') {
+    response.end('<h1>Hello!</h1>');
+  } else if (method === 'POST') {
+    response.end('<h1>Hai!</h1>');
+  } else if (method === 'PUT') {
+    response.end('<h1>Bonjour!</h1>');
+  } else if (method === 'DELETE') {
+    response.end('<h1>Salam!</h1>');
+  }
 };
 
 const server = http.createServer(requestListener);
