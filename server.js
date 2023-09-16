52286eb79de4eb1d935e7f2b60085b4ae2e236ab
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-16 10:07:36 +0700

    feat: response body

--- server.js
+++ server.js
@@ -9,17 +9,21 @@ const requestListener = (request, response) => {
   if (url === '/') {
     if (method === 'GET') {
       response.statusCode = 200;
-      response.end('<h1>Ini adalah Homepage</h1>');
+      response.end(JSON.stringify({ message: 'Ini adalah homepage' }));
     } else {
       response.statusCode = 400;
       response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
       );
     }
   } else if (url === '/about') {
     if (method === 'GET') {
       response.statusCode = 200;
-      response.end('<h1>Ini adalah Halaman About</h1>');
+      response.end(
+        JSON.stringify({ message: 'Halo! Ini adalah halaman about' })
+      );
     } else if (method === 'POST') {
       let body = [];
 
@@ -31,17 +35,21 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`<h1>Halo, ${name}! Ini adalah Halaman About</h1>`);
+        response.end(
+          JSON.stringify({ message: `Halo, ${name}! Ini adalah halaman about` })
+        );
       });
     } else {
       response.statusCode = 400;
       response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
       );
     }
   } else {
     response.statusCode = 404;
-    response.end('<h1>Halaman tidak ditemukan!</h1>');
+    response.end(JSON.stringify({ message: 'Halaman tidak ditemukan!' }));
   }
 };
 
